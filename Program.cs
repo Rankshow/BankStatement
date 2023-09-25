ef077c5e8c813f60210a5007d9aa12276eaefbaa
rankshow <uwaomaobinna20@gmail.com>
2023-09-25 21:19:52 +0100

    store to a console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO;
+using System.Linq;
+using System.Globalization;
 using BankStatement;
 
 bool status = true;
@@ -6,29 +8,32 @@ bool status = true;
 // * loop while the status is true
 while(status)  
 {
-    AccountDetails accoutState = new AccountDetails();
-    Console.WriteLine("Please provide us your account name");
-    accoutState.Name = Console.ReadLine();
+        AccountDetails accoutState = new AccountDetails();
+        Console.WriteLine("Please provide us your account name");
+        accoutState.Name = Console.ReadLine();
 
-    Console.WriteLine($"Good Evening! {accoutState.Name}, Tell us how much you withdrawe last");
-    accoutState.Withdrawal = Console.ReadLine();  
+        Console.WriteLine($"Good Evening! {accoutState.Name}, Tell us how much you withdrawe last");
+        accoutState.Withdrawal = Console.ReadLine();  
 
-    Console.WriteLine("Enter our account number");
-    accoutState.AccountNumber = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter our account number");
+        accoutState.AccountNumber = Convert.ToDouble(Console.ReadLine());
 
-    Console.WriteLine("Please, How much did you deposited last?");
-    accoutState.Deposit = Console.ReadLine();
+        Console.WriteLine("Please, How much did you deposited last?");
+        accoutState.Deposit = Console.ReadLine();
 
-    Console.WriteLine("What is your last balance?");
-    accoutState.Balance = Console.ReadLine();
-
-    // * creating and storing the datas that is been supply in this file
-    string? filePath = @"C:\Bank\BankData.txt";
+        Console.WriteLine("What is your last balance?");
+        accoutState.Balance = Console.ReadLine();
 
     try
-    {
-        File.WriteAllText(filePath, accoutState.ToString());  
-    }
+    { 
+        // ! storing the data in the console.........
+        Console.WriteLine($"Id : { accoutState.Id} ");
+        Console.WriteLine($"Name : {accoutState.Name}");
+        Console.WriteLine($"Account number : {accoutState.AccountNumber}");
+        Console.WriteLine($"Withdrawal : {accoutState.Withdrawal}");
+        Console.WriteLine($"Deposit : {accoutState.Deposit}");
+        Console.WriteLine($"Balance : {accoutState.Balance}");
+     }
     catch(Exception e)
     {
         Console.WriteLine("Sorry we can find your file:" + e.ToString());
